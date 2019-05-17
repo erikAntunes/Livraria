f3778e120f340994a2609c9b3b7a2ef543bf896d
erikAntunes <erik.antunes@live.com>
2019-05-17 18:05:43 -0300

    u.u Triste Commit - não dei push lá na DigitalHouse

--- src/Catalogo.java
+++ src/Catalogo.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 
-public class Catalogo extends Livro {
+public class Catalogo {
 
     public List<Livro> estoque;
 
@@ -18,37 +18,39 @@ public class Catalogo extends Livro {
         estoque.add(l1);
 
         Livro l2 = new Livro();
-        l1.setTitulo("Filhos do Éden: Herdeiros de Atlântida");
-        l1.setAnoLancamento(2011);
-        l1.setAutor("Eduardo Spohr");
-        l1.setCodigo("0002");
-        l1.setCodigoISBN("9788576862079");
-        l1.setPreco(74.90F);
-        l1.setQuantidadeEmEstoque(10);
+        l2.setTitulo("Filhos do Éden: Herdeiros de Atlântida");
+        l2.setAnoLancamento(2011);
+        l2.setAutor("Eduardo Spohr");
+        l2.setCodigo("0002");
+        l2.setCodigoISBN("9788576862079");
+        l2.setPreco(74.90F);
+        l2.setQuantidadeEmEstoque(10);
         estoque.add(l2);
 
         Livro l3 = new Livro();
-        l1.setTitulo("Filhos do Éden: Anjos da Morte");
-        l1.setAnoLancamento(2013);
-        l1.setAutor("Eduardo Spohr");
-        l1.setCodigo("0003");
-        l1.setCodigoISBN("9788576862451");
-        l1.setPreco(27.40F);
-        l1.setQuantidadeEmEstoque(10);
+        l3.setTitulo("Filhos do Éden: Anjos da Morte");
+        l3.setAnoLancamento(2013);
+        l3.setAutor("Eduardo Spohr");
+        l3.setCodigo("0003");
+        l3.setCodigoISBN("9788576862451");
+        l3.setPreco(27.40F);
+        l3.setQuantidadeEmEstoque(10);
         estoque.add(l3);
 
         Livro l4 = new Livro();
-        l1.setTitulo("Filhos do Éden: Paraíso Perdido");
-        l1.setAnoLancamento(2015);
-        l1.setAutor("Eduardo Spohr");
-        l1.setCodigo("0004");
-        l1.setCodigoISBN("9788576864752");
-        l1.setPreco(28.40F);
-        l1.setQuantidadeEmEstoque(10);
+        l4.setTitulo("Filhos do Éden: Paraíso Perdido");
+        l4.setAnoLancamento(2015);
+        l4.setAutor("Eduardo Spohr");
+        l4.setCodigo("0004");
+        l4.setCodigoISBN("9788576864752");
+        l4.setPreco(28.40F);
+        l4.setQuantidadeEmEstoque(10);
         estoque.add(l4);
     }
 
-    public void cadastrarLivro(Livro l){}
+    public void cadastrarLivro(Livro livro){
+        estoque.add(livro);
+    }
 
     public void consultarLivroPorCodigo(String codigo){}
 
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
-public class Main extends Livro{
+public class Main {
 
     public static void main(String[] args) {
 
 
+        Catalogo catalogoDeLivros = new Catalogo();
+
+
+
+        System.out.println(catalogoDeLivros.estoque.get(0).getAutor());
 
     }
 }
