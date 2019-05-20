68d86ad9c37df1e0426466634067b700054d8db7
erikAntunes <erik.antunes@live.com>
2019-05-19 21:10:18 -0300

    Preciso correr pra assistir GOT

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,55 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Livro l5 = new Livro();
+        l5.setTitulo("A Torre das Almas");
+        l5.setAutor("Eduardo Spohr");
+        l5.setAnoLancamento(2012);
+        l5.setCodigo("0005");
+        l5.setQuantidadeEmEstoque(10);
+        l5.setCodigoISBN("9788562942686");
+        l5.setPreco(49.90F);
 
         Catalogo catalogoDeLivros = new Catalogo();
 
-        // se o livro foi encontrado?
-        //Livro livro = catalogoDeLivros.consultarLivroPorCodigo("0001");
-        //if (livro != null) {
-          //  System.out.println(livro.getTitulo());
-        //} else {
-     //       System.out.println("Livro não encontrado!");
+        catalogoDeLivros.cadastrarLivro(l5);
 
+        System.out.println("Temos os seguintes livros disponiveis: ");
+
+        System.out.println("******************************************");
+
+        System.out.println("1- "+catalogoDeLivros.consultarLivroPorCodigo("0001").getTitulo());
+
+        System.out.println("2- "+catalogoDeLivros.consultarLivroPorCodigo("0002").getTitulo());
+
+        System.out.println("3- "+catalogoDeLivros.consultarLivroPorCodigo("0003").getTitulo());
+
+        System.out.println("4- "+catalogoDeLivros.consultarLivroPorCodigo("0004").getTitulo());
+
+        System.out.println("5- "+catalogoDeLivros.consultarLivroPorCodigo("0005").getTitulo());
+
+        System.out.println("*******************************************************************************************");
+
+        catalogoDeLivros.efetuarVendaPorCodigo("0001");
+        System.out.println(" ");
+        System.out.println("*******************************************************************************************");
+        catalogoDeLivros.efetuarVendaPorCodigo("0003");
+        System.out.println(" ");
+        System.out.println("*******************************************************************************************");
+        catalogoDeLivros.efetuarVendaPorCodigo("0002");
+        System.out.println(" ");
+        System.out.println("*******************************************************************************************");
         catalogoDeLivros.efetuarVendaPorCodigo("0004");
-        }
+        System.out.println(" ");
+        System.out.println("*******************************************************************************************");
+        catalogoDeLivros.efetuarVendaPorCodigo("0005");
+        System.out.println(" ");
+        System.out.println("*******************************************************************************************");
+        catalogoDeLivros.efetuarVendaPorCodigo("0003");
+        System.out.println(" ");
+        System.out.println("*******************************************************************************************");
+        catalogoDeLivros.efetuarVendaPorCodigo("0003");
+        System.out.println(" ");
+        System.out.println("*******************************************************************************************");
     }
+}
